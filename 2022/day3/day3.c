2351795fcd1bc3c97d27a38c77ad850c576a87a0
Edoardo Gennaretti <edo.gennaretti@gmail.com>
2022-12-05 22:06:48 +0100

    day 3

--- 2022/day3/day3.c
+++ 2022/day3/day3.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #define FILE_NAME "2022/day3/input.txt"
 #define STR_LEN 80
 
-int strLen(char *str){
-
-}
 
 int itemToPriority(char item){
     if(item >= 'a' && item <= 'z')
@@ -37,12 +33,36 @@ char itemInBothRucksack(char *str){
     return 0;
 }
 
+char commonItemIn3Rucksack(char* str1, char* str2, char* str3){
+    int count1[128] = {0};
+    int count2[128] = {0};
+    int count3[128] = {0};
+
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+    int len3 = strlen(str3);
+
+    for(int i = 0; i < len1; i++)
+        count1[str1[i]]++;
+
+    for(int i = 0; i < len2; i++)
+        count2[str2[i]]++;
+
+    for(int i = 0; i < len3; i++)
+        count3[str3[i]]++;
+
+    for(int i = 0; i < 128; i++){
+        if(count1[i] != 0 && count2[i] != 0 && count3[i] != 0){
+            return (char)i;
+        }
+    }
+}
+
 
 
 int partOne(){
     char str[STR_LEN];
     int myScore = 0;
-    int po;
 
     FILE *fp = fopen(FILE_NAME, "r");
 
@@ -64,8 +84,37 @@ int partOne(){
     return 0;
 }
 
+int partTwo(){
+    char str1[STR_LEN];
+    char str2[STR_LEN];
+    char str3[STR_LEN];
+    int myScore = 0;
+
+    FILE *fp = fopen(FILE_NAME, "r");
+
+    if(fp == NULL){
+        printf("Error opening the file\n");
+        return -1;
+    }
+
+    fscanf(fp, " %s", str1);
+    fscanf(fp, " %s", str2);
+    fscanf(fp, " %s", str3);
+    while(!feof(fp)){
+        myScore += itemToPriority(commonItemIn3Rucksack(str1,str2,str3));
+
+        fscanf(fp, " %s", str1);
+        fscanf(fp, " %s", str2);
+        fscanf(fp, " %s", str3);
+    }
+
+
+    printf("My score: %d", myScore);
+
+    return 0;
+}
 
 
 int main(){
-    return partOne();
+    return partTwo();
 }
