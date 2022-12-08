844d2fd5f8ebfa8961dd65c69ce44ffcc6f140d0
Edoardo Gennaretti <edo.gennaretti@gmail.com>
2022-12-08 16:48:05 +0100

    day 6

---
+++ 2022/day6/day6.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define FILE_NAME "2022/day6/input.txt"
+#define STR_LEN 5000
+#define WIN_LEN 14
+
+int noDuplicates(char* str, int dim){
+   for(int i = 1; i < dim; i++){
+        for(int j = 0; j < i; j++){
+            if(str[i] == str[j])
+                return 0;
+        }
+    }
+
+    return 1;
+}
+
+int analizeString(char* str){
+    for(int i = 0; str[i+WIN_LEN] != '\0'; i++){
+        if(noDuplicates(str+i, WIN_LEN)){
+            return i + WIN_LEN;
+        }
+    }
+
+    return -1;
+}
+
+int main(){
+    char str[STR_LEN];
+
+    FILE *fp = fopen(FILE_NAME, "r");
+
+    if(fp == NULL){
+        printf("Error opening the file\n");
+        return -1;
+    }
+
+    fgets(str, STR_LEN, fp);
+    while(!feof(fp)){
+        printf("Result: %d", analizeString(str));
+
+        fgets(str, STR_LEN, fp);
+    }
+
+    fclose(fp);
+
+    return 0;
+}
