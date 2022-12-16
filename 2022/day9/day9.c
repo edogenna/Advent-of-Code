e9fa248ea5024d156352a7b9b2da790335b446b0
Edoardo Gennaretti <edo.gennaretti@gmail.com>
2022-12-16 11:50:18 +0100

    day 9

--- 2022/day9/day9.c
+++ 2022/day9/day9.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#include <string.h>
-#include <string.h>
 #include <stdlib.h>
 
 #define FILE_NAME "2022/day9/input.txt"
 #define DIM 3000
 #define STR_LEN 30
 #define STARTING_POINT (DIM/2)
+#define N_NODE 10
 
 int matr[DIM][DIM];
 int tailRow, tailCol, headRow, headCol;
+//head is in pos 0
+int posRow[N_NODE], posCol[N_NODE];
 
 
 int isTailNearHead(){
@@ -19,6 +20,72 @@ int isTailNearHead(){
                 return 1;
 
     return 0;
+}
+int isNodeNearThePrevious(int node){
+    if(node == 0){
+        printf("You are checking the head!\n");
+        return 1;
+    }
+
+    for(int i = -1; i <= 1; i++)
+        for(int j = -1; j <= 1; j++)
+            if(((posRow[node] + i) == posRow[node-1]) && ((posCol[node] + j) == posCol[node-1]))
+                return 1;
+
+    return 0;
+}
+void adjustNodes(){
+    for(int i = 1; i < N_NODE; i++){
+        if(!isNodeNearThePrevious(i)){
+            if(posRow[i-1] > posRow[i]){
+                posRow[i]++;
+            }else if(posRow[i-1] < posRow[i]){
+                posRow[i]--;
+            }
+
+            if(posCol[i-1] > posCol[i]){
+                posCol[i]++;
+            }else if(posCol[i-1] < posCol[i]){
+                posCol[i]--;
+            }
+        }
+    }
+
+    if(matr[posRow[N_NODE-1]][posCol[N_NODE-1]] == 0)
+        matr[posRow[N_NODE-1]][posCol[N_NODE-1]] = 1;
+}
+
+void moveHeadUpLong(){
+    if(posRow[0] == 0)
+        printf("ERROR HEAD UP\n");
+
+    posRow[0]--;
+
+    adjustNodes();
+}
+void moveHeadDownLong(){
+    if(posRow[0] == DIM-1)
+        printf("ERROR HEAD DOWN\n");
+
+    posRow[0]++;
+
+    adjustNodes();
+}
+void moveHeadLeftLong(){
+    if(posCol[0] == 0)
+        printf("ERROR HEAD LEFT\n");
+
+    posCol[0]--;
+
+    adjustNodes();
+}
+void moveHeadRightLong(){
+    if(posCol[0] == DIM-1)
+        printf("ERROR HEAD RIGHT\n");
+
+    posCol[0]++;
+
+    adjustNodes();
 }
 
 void moveHeadUp(){
@@ -88,14 +155,6 @@ int countPositions(){
     return count;
 }
 
-void printMatrix(){
-    for(int i = 0; i < DIM; i++){
-        for(int j = 0; j < DIM; j++)
-            printf("%d",matr[i][j]);
-        printf("\n");
-    }
-}
-
 int partOne(){
     for(int i = 0; i < DIM; i++)
         for(int j = 0; j < DIM; j++)
@@ -122,7 +181,6 @@ int partOne(){
         direction = str[0];
         numMoves = atoi(str + 2);
 
-        //printf("ho letto;%s\n", str);
         for(int i = 0; i < numMoves; i++){
             switch (direction) {
                 case 'U':
@@ -140,20 +198,70 @@ int partOne(){
                 default:
                     printf("Error direction\n");
             }
-
-            //printMatrix();
         }
-
-
         fgets(str,STR_LEN,fp);
     }
 
     printf("Positions visited by the rope: %d", countPositions());
 
+    return 0;
+}
+
+int partTwo(){
+    for(int i = 0; i < DIM; i++)
+        for(int j = 0; j < DIM; j++)
+            matr[i][j] = 0;
+
+    for(int i = 0; i < N_NODE; i++){
+        posRow[i] = STARTING_POINT;
+        posCol[i] = STARTING_POINT;
+    }
+
+    matr[posRow[N_NODE-1]][posCol[N_NODE-1]] = 1;
+
+    char str[STR_LEN];
+    int numMoves;
+    char direction;
+
+    FILE* fp = fopen(FILE_NAME, "r");
+    if(fp == NULL){
+        printf("Error opening the file\n");
+        return -1;
+    }
+
+    fgets(str,STR_LEN,fp);
+
+    while(!feof(fp)){
+        direction = str[0];
+        numMoves = atoi(str + 2);
+
+        for(int i = 0; i < numMoves; i++){
+            switch (direction) {
+                case 'U':
+                    moveHeadUpLong();
+                    break;
+                case 'D':
+                    moveHeadDownLong();
+                    break;
+                case 'L':
+                    moveHeadLeftLong();
+                    break;
+                case 'R':
+                    moveHeadRightLong();
+                    break;
+                default:
+                    printf("Error direction\n");
+            }
+
+        }
+        fgets(str,STR_LEN,fp);
+    }
+
+    printf("Positions visited by the rope: %d", countPositions());
 
     return 0;
 }
 
 int main(){
-    return partOne();
+    return partTwo();
 }
